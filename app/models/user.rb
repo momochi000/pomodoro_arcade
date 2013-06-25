a30c38dbcd51a831bba12d04605d103a6fafbc43
Zachery Chin <zacherychin@yahoo.com>
2013-06-24 23:25:16 -0700

    create user profile model and associate with user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_many :pomodoros
+  has_one :user_profile
 
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
