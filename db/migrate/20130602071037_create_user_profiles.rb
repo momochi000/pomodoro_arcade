de6feb130c65774e157c4ba16b7698dba29eb61d
Zachery Chin <zacherychin@yahoo.com>
2013-06-24 23:24:11 -0700

    Create user profile table

---
+++ db/migrate/20130602071037_create_user_profiles.rb
@@ -0,0 +1,11 @@
+class CreateUserProfiles < ActiveRecord::Migration
+  def change
+    create_table :user_profiles do |t|
+
+      t.string :first_name
+      t.string :last_name
+
+      t.timestamps
+    end
+  end
+end
