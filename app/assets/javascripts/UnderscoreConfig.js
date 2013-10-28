8a4835e22a6d9baa08993186b67fceb6e133bb4f
momochi000 <zacherychin@yahoo.com>
2013-10-27 19:19:54 -0700

    Note to self to update underscore template delims

--- app/assets/javascripts/UnderscoreConfig.js
+++ app/assets/javascripts/UnderscoreConfig.js
@@ -1,3 +1,7 @@
 _.templateSettings = {
   interpolate: /\{\{(.+?)\}\}/g
+  // TODO: add appropriate regexes to output these, 
+  //   we can't use erb style delims because rails will look for rails vars/vals
+  //escape: //g,
+  //evaluate: //g
 };
