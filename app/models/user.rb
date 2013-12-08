27ecb27d759d294b002528f1b096d5936984a403
momochi000 <zacherychin@yahoo.com>
2013-12-08 11:22:00 -0800

    refactor completed pomodoros method to not be a relation
    the actual relation from user to completed whatever
    is completed events.  The answer of how many completed
    pomodoros does user x have is extrapolated from the
    number of completed events.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_many :activity_timers
-  has_many :completed_pomodoros, :through => :activity_timers
+  has_many :completed_events, :through => :activity_timers
   has_one :user_profile
 
   # Include default devise modules. Others available are:
@@ -12,7 +12,12 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model #DEPRECATED for rails 4
   #attr_accessible :email, :password, :password_confirmation, :remember_me
 
+  def completed_pomodoros #Alias for completed_events relation
+    completed_events
+  end
+
   def timers #Alias for activity_timers relation
     activity_timers
   end
+
 end
