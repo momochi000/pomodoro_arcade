8067cc9766e7da547d070183695af3b55abf7b8b
momochi000 <zacherychin@yahoo.com>
2014-02-01 21:52:27 -0800

    Add logged_in? utility method.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
 
   before_filter :configure_permitted_parameters, :if => :devise_controller?
 
+  def logged_in?
+    current_user.present?
+  end
+
   private
 
   def configure_permitted_parameters
