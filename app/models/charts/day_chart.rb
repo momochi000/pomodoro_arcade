dbb117da56cb037a4a68c2ae5668787e212d5ae6
momochi000 <zacherychin@yahoo.com>
2014-02-19 20:14:06 -0800

    Add goal tracking to daily progress graph

--- app/models/charts/day_chart.rb
+++ app/models/charts/day_chart.rb
@@ -2,28 +2,58 @@ module Charts
   class DayChart
     attr_accessor :timers, :date, :today
 
-    def initialize(timers, date=nil)
+    def initialize(timers, date=nil, options={})
       @timers = timers
       @today = true unless date
       @date = date || Time.now
     end
 
     def to_hash
+      data
+    end
+
+    def to_json
+      to_hash.to_json
+    end
+
+    private
+
+    def data
+      build_data unless @data
+      @data
+    end
+
+    def build_data
+      @data = []
+      @data << build_timers_complete_on_date
+      @data << build_timer_goals_on_date
+    end
+
+    def build_timers_complete_on_date
       output = {}
       output['key'] = (today ? 'Pomodoros completed' : "Pomodoros completed on date ___") # TODO: fix the title when given a date
       output['color'] = '#d67777'
-      values = timers.map do |t|
+      output['values'] = timers.map do |t|
         {
           :label => t.title,
           :value => t.number_completed_on_day(date)
         }
       end
-      output['values'] = values
-      [output]
+      output
     end
 
-    def to_json
-      to_hash.to_json
+    def build_timer_goals_on_date
+      output = {}
+      output['key'] = (today ? 'Goals' : "Pomodoros goals on date ___") # TODO: fix the title when given a date
+
+      output['color'] = 'black'
+      output['values'] = timers.map do |t|
+        {
+          :label => t.title,
+          :value => t.goal.value
+        }
+      end
+      output
     end
   end
 end
